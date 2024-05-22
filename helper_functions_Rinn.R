bac1889f6adccb37c5c22d8962a5f2b3ccfde579
luiza-yuan <luizashen58@gmail.com>
2024-05-22 19:19:15 +0200

    changed file path name to allow decimals for sf (not just integers)

--- helper_functions_Rinn.R
+++ helper_functions_Rinn.R
@@ -2231,7 +2231,7 @@ setup_filepaths <- function(filepath_est,
     sprintf("sf%s", sf), # added
     sprintf("tau%s", ntau), # added
     sprintf(
-      "D2strength%.4f_sf%d_N%d_iter%04d_step%04d_pars%.2f_%.2f_%.2f_%.2f_%.2f_%.2f_%.2f_bins%d_ntau%d_interpol%d_bw%.2f_Downsampled_from_sf%d_N%d.RDS",
+      "D2strength%.4f_sf%.2f_N%d_iter%04d_step%04d_pars%.2f_%.2f_%.2f_%.2f_%.2f_%.2f_%.2f_bins%d_ntau%d_interpol%d_bw%.2f_Downsampled_from_sf%d_N%d.RDS",
       strength_D2,
       sf,
       N, 
